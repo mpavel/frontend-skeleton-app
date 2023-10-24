2635a24303dfa555a6dabb366d34314a47222e7e
Pavel Mocan <pavel-ovidiu.mocan@erstegroup.com>
2023-10-24 12:33:26 +0200

    Setup unit testing with Vitest and update configurations and scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5174,
+    strictPort: true,
+  },
   plugins: [react()],
+  test: {
+    globals: false,
+  },
   resolve: {
     alias: {
       'src/': 'src/',
